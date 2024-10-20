e531e9cd08488c887b38c2505271f3c0a55c284a
ishanladva <ishanladva0007@gmail.com>
2024-10-20 17:00:40 +0530

    SET UP DEPLOY FILE

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/string-calculator-kata/",
   test: {
     environment: "jsdom",
     globals: true,
